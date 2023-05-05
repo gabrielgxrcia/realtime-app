3550e56f3eece8e131a3f431e813c708bd58f157
Gabriel Garcia <gabrielgrazeffi123@gmail.com>
2023-05-05 15:52:07 -0300

    add: auth google/components

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from '@/components/ui/Button'
+import { FC } from 'react'
+
+interface pageProps {}
+
+const Page: FC<pageProps> = ({}) => {
+  return <Button>teste</Button>
+}
+
+export default Page

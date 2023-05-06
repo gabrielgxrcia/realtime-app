bc94ff459ac6bd7349a3651cbcb40e5948d127b8
Gabriel Garcia <gabrielgrazeffi123@gmail.com>
2023-05-06 04:17:52 -0300

    add: middleware/refactoring accept deny

--- src/app/(dashboard)/dashboard/requests/page.tsx
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -1,3 +1,4 @@
+import FriendRequests from '@/components/FriendRequests'
 import { fetchRedis } from '@/helpers/redis'
 import { authOptions } from '@/lib/auth'
 import { getServerSession } from 'next-auth'
@@ -7,6 +8,34 @@ import { FC } from 'react'
 const page = async () => {
   const session = await getServerSession(authOptions)
   if (!session) notFound()
+
+  //ID das pessoas que enviaram solicitações de amigos do usuário conectado.
+  const incomingSenderIds = (await fetchRedis(
+    'smembers',
+    `user:${session.user.id}:incoming_friend_requests`
+  )) as string[]
+
+  const incomingFriendRequests = await Promise.all(
+    incomingSenderIds.map(async senderId => {
+      const sender = (await fetchRedis('get', `user:${senderId}`)) as User
+      return {
+        senderId,
+        senderEmail: sender.email,
+      }
+    })
+  )
+
+  return (
+    <main className="pt-8">
+      <h1 className="font-bold text-5xl mb-8">Adicionar um amigo</h1>
+      <div className="flex flex-col gap-4">
+        <FriendRequests
+          incomingFriendRequests={incomingFriendRequests}
+          sessionId={session.user.id}
+        />
+      </div>
+    </main>
+  )
 }
 
-//ID das pessoas que enviaram solicitações de amigos do usuário conectado.
+export default page

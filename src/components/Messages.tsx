6ef1018defe4bee719bde2b6b4fc600242911d46
Gabriel Garcia <gabrielgrazeffi123@gmail.com>
2023-05-06 16:43:25 -0300

    styling improvements

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -3,14 +3,22 @@
 import { cn } from '@/lib/utils'
 import { Message } from '@/lib/validations/message'
 import { format, utcToZonedTime } from 'date-fns-tz'
+import Image from 'next/image'
 import { FC, useRef, useState } from 'react'
 
 interface MessagesProps {
   initialMessages: Message[]
   sessionId: string
+  sessionImg: string | null | undefined
+  chatPartner: User
 }
 
-const Messages: FC<MessagesProps> = ({ initialMessages, sessionId }) => {
+const Messages: FC<MessagesProps> = ({
+  initialMessages,
+  sessionId,
+  chatPartner,
+  sessionImg,
+}) => {
   const [messages, setMessages] = useState<Message[]>(initialMessages)
 
   const scrollDownRef = useRef<HTMLDivElement | null>(null)
@@ -69,6 +77,24 @@ const Messages: FC<MessagesProps> = ({ initialMessages, sessionId }) => {
                     {formatTimestamp(message.timestamp)}
                   </span>
                 </span>
+              </div>
+
+              <div
+                className={cn('relative w-6 h-6', {
+                  'order-2': isCurrentUser,
+                  'order-1': !isCurrentUser,
+                  invisible: hasNextMessageFromSameUser,
+                })}
+              >
+                <Image
+                  fill
+                  src={
+                    isCurrentUser ? (sessionImg as string) : chatPartner.image
+                  }
+                  alt="Foto de perfil"
+                  referrerPolicy="no-referrer"
+                  className="rounded-full"
+                />
               </div>
             </div>
           </div>
